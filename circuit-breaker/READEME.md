6cd818d4c23587244a20aaf02b16b9d54b75566f
asafr <asaf.rehavi@gmail.com>
2023-07-01 15:41:47 +0300

    CircuitBreaker

---
+++ circuit-breaker/READEME.md
@@ -0,0 +1,14 @@
+#This is simple example of use circuit-breaker
+At this example at the index.js file we can see GET request that ask for data for http request
+<br />
+prerequisite :
+<br />
+1)clone this repo
+<br />
+run npm i 
+<br />
+run node index.js from the circuit-breaker folder 
+<br />
+
+<br />
+look at the server console
---
+++ circuit-breaker/index.js
@@ -0,0 +1,32 @@
+//thamks to https://www.npmjs.com/package/opossum
+
+const CircuitBreaker = require('opossum');
+const http = require('http');
+
+function asyncFunctionThatCouldFail(abortSignal, x, y) {
+  return new Promise((resolve, reject) => {
+    http.get(
+      'http://jsonplaceholder.typicode.com/users',
+      { signal: abortSignal },
+      (res) => {
+        if(res.statusCode < 300) {
+          resolve(res.statusCode);
+          return;
+        }
+
+        reject(res.statusCode);
+      }
+    );
+  });
+}
+
+const abortController = new AbortController();
+const options = {
+  abortController,
+  timeout: 30000, // If our function takes longer than 3 seconds, trigger a failure
+};
+const breaker = new CircuitBreaker(asyncFunctionThatCouldFail, options);
+
+breaker.fire(abortController.signal)
+  .then(console.log)
+  .catch(console.error);
